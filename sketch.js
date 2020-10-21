7664ec39f13b8ebafbe3cffaf1d9a994d06051df
marie-bella <nandini.tikarya@gmail.com>
2020-10-21 16:57:27 +0530

    created a chain

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var log6;
 
 var platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,7 +39,12 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6=new Log(250,180,80,PI/2);
+
     bird = new Bird(100,100);
+
+   chain= new Chain(bird.body,log6.body);
+
 
 }
 
@@ -61,6 +69,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+    chain.display();
+
+    
 }
